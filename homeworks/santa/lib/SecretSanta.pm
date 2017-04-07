bb2e924511652073d6eeafa8fe73940cd5e09f19
timurbaev <timurbaev@gmail.com>
2017-04-07 18:13:31 +0300

    Update SecretSanta.pm

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -6,11 +6,23 @@ use warnings;
 use DDP;
 
 sub calculate {
-	my @members = @_;
+	my @members;
 	my @res;
-	# ...
-	#	push @res,[ "fromname", "toname" ];
-	# ...
+	my %couples;
+	foreach my $member (@_) {
+		if (ref $member) {
+			push @members, $couples{$member->[1]} = $member->[0];
+			push @members, $couples{$member->[0]} = $member->[1];
+		}
+		else {
+			push @members, $member;
+		}	
+	}
+	foreach my $person (@members) {
+		my $candidate = $members[int(rand(@members))];
+		redo if ((exists $couples{$person}) && ($couples{$person} eq $candidate) || ($person eq $candidate));
+		push @res, [$person, $candidate];
+	}
 	return @res;
 }
 
