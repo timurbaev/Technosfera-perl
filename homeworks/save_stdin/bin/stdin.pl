9f0d89c256e26839c776a259bff578ddd5544ab5
timurbaev <timurbaev@gmail.com>
2017-04-21 15:35:50 +0300

    Update stdin.pl

--- homeworks/save_stdin/bin/stdin.pl
+++ homeworks/save_stdin/bin/stdin.pl
@@ -4,5 +4,27 @@ use strict;
 use warnings;
 use Getopt::Long;
 
-...
-
+my ($length, $count, $exit, $fh) = (0, 0);
+$SIG{'INT'} = sub {
+	if ($exit) {
+		printf ("%d %d %d", $length, $count, $length / $count) unless $count == 0;
+		close ($fh);
+		exit 0;
+	}
+	warn "Double Ctrl+C for exit";
+	$exit = 1;
+};
+print "Get ready\n";
+my $file;
+GetOptions ("file=s" => \$file);
+die unless defined $file;
+open($fh, '>', $file) or die $!;
+while (<>) {
+	$exit = 0;
+	$count++;
+	print $fh $_;
+	chomp;
+	$length += length $_;
+}
+$exit = 1;
+$SIG{'INT'}->();
