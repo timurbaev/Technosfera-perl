55ee698a390a73a53777699eef7c9d610cd82b60
timurbaev <timurbaev@gmail.com>
2017-05-09 11:33:14 +0300

    Update myconst.pm

--- homeworks/myconst/lib/myconst.pm
+++ homeworks/myconst/lib/myconst.pm
@@ -3,7 +3,7 @@ package myconst;
 use strict;
 use warnings;
 use Scalar::Util 'looks_like_number';
-
+use Switch;
 
 =encoding utf8
 
@@ -37,6 +37,35 @@ print ZERO;             # 0
 print PI;               # 3.14
 =cut
 
-
+sub import {
+	my ($caller) = (caller, shift);
+	map {die unless defined} @_;
+	my %hash = @_ unless @_ % 2 and die;
+	my @import;
+	while (my ($key, $value) = each %hash){
+		die if ref $key eq "ARRAY" or ref $key eq "HASH" or not defined $key or looks_like_number ($key) or not $key =~ /^\w+$/ or ref $value eq "ARRAY";
+		map {die if ref $value->{$_} eq "ARRAY" or ref $value->{$_} eq "HASH" or looks_like_number($_) or not $_=~ /^\w+$/} keys %{$value} if (ref $value eq "HASH");
+		switch(ref $value){
+			case "" {push @import, {name => $key, value => $value, group => "all"}}
+			case "HASH" {push @import, {name => $_, value => $value->{$_}, group => $key} foreach (keys %{$value})}
+			else {die;}
+		}
+	}
+	no strict "refs";
+	no warnings;
+	map {my $import = $_; *{"$caller::$import->{name}"} = sub(){$import->{value}};} @import;
+	*{$caller."::import"} = sub (){
+		my ($caller) = (caller, shift);
+		foreach my $string (@_){
+			if($string =~ /^:all/){
+				map {my $import = $_; *{"$caller::$import->{name}"} = sub(){$import->{value}}} @import;
+			}elsif($string =~ /^:/){
+				map{my $import = $_; *{"$caller::$import->{name}"} = sub(){$import->{value}}} (grep {$string eq ":$_->{group}"} @import);
+			}else{
+				map{my $import = $_; *{"$caller::$string"} = sub(){$import->{value}} if ($string eq $import->{name})} @import;
+			}
+		}
+	};
+}
 
 1;
